f774a9bf98f355992ad06bd82a44de07cec6ad6b
krishnakandula <krishna.c.kandula@gmail.com>
2021-11-08 20:29:07 -0600

    Add SphereGenerator

--- Assets/Scripts/SphereGenerator.cs
+++ Assets/Scripts/SphereGenerator.cs
@@ -3,10 +3,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CircleGenerator : MonoBehaviour
+public class SphereGenerator : MonoBehaviour
 {
     [SerializeField] private GameObject pointPrefab;
-    [SerializeField] [Range(2, 360)] private int granularity = 30;
+    [SerializeField] [Range(2, 360)] private int yawGranularity = 30;
+    [SerializeField] [Range(2, 360)] private int pitchGranularity = 30;
     [SerializeField] [Range(0.5f, 100f)] private float radius = 5;
     [SerializeField] [Range(0f, 0.5f)] private float pointCreationDelay = .05f;
     [SerializeField] [Range(-19f, 20f)] private float zOffset = 10f;
@@ -21,7 +22,8 @@ public class CircleGenerator : MonoBehaviour
             DestroyOldPoints();
             ClearPointCreationCoroutines();
             coroutines.Add(
-                StartCoroutine(GeneratePointsOnCircle(granularity, radius, pointCreationDelay, yOffset, zOffset)));
+                StartCoroutine(GeneratePointsOnCircle(pitchGranularity, yawGranularity, radius, pointCreationDelay,
+                    yOffset, zOffset)));
         }
     }
 
@@ -44,7 +46,8 @@ public class CircleGenerator : MonoBehaviour
         coroutines.Clear();
     }
 
-    private IEnumerator GeneratePointsOnCircle(int granularity, float radius, float pointCreationDelay, float yOffset,
+    private IEnumerator GeneratePointsOnCircle(int pitchGranularity, int yawGranularity, float radius,
+        float pointCreationDelay, float yOffset,
         float zOffset)
     {
         if (pointPrefab == null)
@@ -53,28 +56,38 @@ public class CircleGenerator : MonoBehaviour
             yield return null;
         }
 
-        float[] angles = GenerateAngles(granularity);
-        for (int i = 0; i < angles.Length; ++i)
+        float[,] angles = GenerateAngles(pitchGranularity, yawGranularity);
+        for (int i = 0; i < angles.GetLength(0); ++i)
         {
-            float angle = angles[i];
-            double angleInRadians = angle * (Math.PI / 180);
-            double xCoord = radius * Math.Cos(angleInRadians);
-            double yCoord = radius * Math.Sin(angleInRadians);
+            float pitchAngle = angles[i, 0];
+            float yawAngle = angles[i, 1];
+            double pitchAngleInRadians = pitchAngle * (Math.PI / 180);
+            double yawAngleInRadians = yawAngle * (Math.PI / 180);
 
-            Vector3 pos = new Vector3((float) xCoord, (float) (yCoord + yOffset), zOffset);
+            double xCoord = radius * Math.Cos(pitchAngleInRadians) * Math.Cos(yawAngleInRadians);
+            double zCoord = radius * Math.Cos(pitchAngleInRadians) * Math.Sin(yawAngleInRadians);
+            double yCoord = radius * Math.Sin(pitchAngleInRadians);
+
+            Vector3 pos = new Vector3((float) xCoord, (float) (yCoord + yOffset), (float) (zCoord + zOffset));
             CreatePoint(pos);
             yield return new WaitForSeconds(pointCreationDelay);
         }
     }
 
 
-    private static float[] GenerateAngles(int granularity)
+    private static float[,] GenerateAngles(int pitchGranularity, int yawGranularity)
     {
-        float[] angles = new float[granularity];
-        float angle = 360f / granularity;
-        for (int i = 0; i < angles.Length; ++i)
+        int numAngles = pitchGranularity * yawGranularity;
+        float[,] angles = new float[numAngles, 2];
+        float pitchAngle = 360f / pitchGranularity;
+        float yawAngle = 360f / yawGranularity;
+        for (int x = 0, i = 0; x < pitchGranularity; ++x)
         {
-            angles[i] = angle * i;
+            for (int y = 0; y < yawGranularity; ++y, ++i)
+            {
+                angles[i, 0] = pitchAngle * x;
+                angles[i, 1] = yawAngle * y;
+            }
         }
 
         return angles;
